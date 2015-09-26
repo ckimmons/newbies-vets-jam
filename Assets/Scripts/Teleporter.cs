2df24b5557d75b6d1c45547c78d1609e3d2c861c
Audrey Cheng <cherryblossoms248@gmail.com>
2015-09-26 15:38:00 -0700

    Added music

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour
+{
+	public Transform teleportTo;
+	
+	void OnCollisionEnter (Collision col) 
+	{
+		col.gameObject.transform.position = teleportTo.position;	
+	}
+
+	void OnTriggerEnter (Collider col) 
+	{
+		col.gameObject.transform.position = teleportTo.position;
+	}
+}
